ac1c2cc3e39c049e770a2711299a61233ae76c19
alafroiskiotos <praetorian@akyroi.gr>
2011-12-17 18:25:30 +0200

    init scripts for the proxy server and the consolce

---
+++ java/proxy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+. ./config/build.conf
+
+LD_LIBRARY_PATH=./jni:$FUSE_HOME/lib $JDK_HOME/bin/java \
+   -classpath .:./config/:./dist/scorpioFS.jar \
+   -Dorg.apache.commons.logging.Log=fuse.logging.FuseLog \
+   -Djava.net.preferIPv4Stack=true \
+    unipi.p2p.chord.util.console.ConsoleServer $*
