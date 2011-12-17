a1dfa46fe93fd31f1dded094ff8ab05b2ea45c19
alafroiskiotos <praetorian@akyroi.gr>
2011-12-17 17:49:22 +0200

    Created console protocol

---
+++ java/src/unipi/p2p/chord/util/console/ConsoleProtocol.java
@@ -0,0 +1,10 @@
+package unipi.p2p.chord.util.console;
+
+public class ConsoleProtocol {
+	public static final int NODE_CREATE = 0;
+	public static final int NODE_STOP = 1;
+	public static final int NODE_STAT = 2;
+	public static final int NODE_ALIVE = 3;
+	public static final int CHORD_PORT = 6788;
+	public static final int PROXY_PORT = 6789;
+}
