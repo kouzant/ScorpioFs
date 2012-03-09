8e56b9a212c2071ac72660c7360327cabe903bd5
alafroiskiotos <praetorian@akyroi.gr>
2012-03-09 13:10:16 +0200

    Threaded console client receiver

--- java/src/unipi/p2p/chord/util/console/ConsoleClientReceiver.java
+++ java/src/unipi/p2p/chord/util/console/ConsoleClientReceiver.java
@@ -7,12 +7,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ConsoleClientReceiver implements Runnable {
 	private static boolean running = true;
-	private LinkedList<Nodes> nodesList;
+	private volatile LinkedList<Nodes> nodesList;
+	private ExecutorService exec = null;
 	public ConsoleClientReceiver(LinkedList<Nodes> nodesList){
 		this.nodesList = nodesList;
+		exec = Executors.newCachedThreadPool();
 	}
 	public void stopRunning(){
 		running = false;
@@ -23,62 +27,83 @@ public class ConsoleClientReceiver implements Runnable {
 		Socket cSocket = null;
 		try{
 			sSocket = new ServerSocket(ConsoleProtocol.CLREC_PORT);
-			BufferedReader bin = null;
 			while(running){
-				int code = -1;
-				int port = -1;
-				int proxyPort = -1;
-				String senderIp = null;
-				bin = null;
 				cSocket = sSocket.accept();
-				bin = new BufferedReader(new InputStreamReader
-						(cSocket.getInputStream()));
-				proxyPort = Integer.parseInt(bin.readLine());
-				code = Integer.parseInt(bin.readLine());
-				port = Integer.parseInt(bin.readLine());
-				senderIp = cSocket.getInetAddress().toString().substring(1);
-				
-				switch(code){
-					case ConsoleProtocol.CREATED:
-						System.out.println("--> Chord node on "+senderIp+" on port "+
-								port+" created successfully");
-						System.out.print("$>");
-						Nodes newNode = new Nodes(senderIp, port, proxyPort);
-						nodesList.add(newNode);
-						break;
-					case ConsoleProtocol.NOT_CREATED:
-						System.out.println("--> Chord node on "+senderIp+" on port "+
-								port+" did not created");
-						System.out.print("$>");
-						break;
-					case ConsoleProtocol.STOPPED:
-						System.out.println("--> Chord node on "+senderIp+" on port "+
-								port+" stopped successfully");
-						System.out.print("$>");
-						Nodes tmpNode = null;
-						Nodes curNode = null;
-						Iterator<Nodes> nodesIt = nodesList.iterator();
-						while(nodesIt.hasNext()){
-							tmpNode = nodesIt.next();
-							if(tmpNode.getIpAddr().equals(senderIp) && tmpNode
-									.getPort() == port){
-								curNode = tmpNode;
-								break;
-							}
-						}
-						nodesList.remove(curNode);
-						break;
-					case ConsoleProtocol.NOT_STOPPED:
-						System.out.println("--> Chord node on "+senderIp+" on port "+
-								port+" did not stopped");
-						System.out.print("$>");
-						break;
+				if(cSocket != null){
+					ThreadedServer tServer = new ThreadedServer(cSocket, nodesList);
+					exec.execute(tServer);
 				}
 			}
-			bin.close();
 			sSocket.close();
 		}catch(IOException e0){
 			e0.printStackTrace();
+		}
+	}
+}
+
+class ThreadedServer implements Runnable{
+	private Socket cSocket = null;
+	private volatile LinkedList<Nodes> nodesList = null;
+	BufferedReader bin = null;
+	
+	public ThreadedServer(Socket cSocket, LinkedList<Nodes> nodesList){
+		this.cSocket = cSocket;
+		this.nodesList = nodesList;
+	}
+	@Override
+	public void run(){
+		int code = -1;
+		int port = -1;
+		int proxyPort = -1;
+		String senderIp = null;
+		bin = null;
+		try{
+		bin = new BufferedReader(new InputStreamReader
+				(cSocket.getInputStream()));
+		proxyPort = Integer.parseInt(bin.readLine());
+		code = Integer.parseInt(bin.readLine());
+		port = Integer.parseInt(bin.readLine());
+		senderIp = cSocket.getInetAddress().toString().substring(1);
+		
+		switch(code){
+			case ConsoleProtocol.CREATED:
+				System.out.println("--> Chord node on "+senderIp+" on port "+
+						port+" created successfully");
+				System.out.print("$>");
+				Nodes newNode = new Nodes(senderIp, port, proxyPort);
+				nodesList.add(newNode);
+				break;
+			case ConsoleProtocol.NOT_CREATED:
+				System.out.println("--> Chord node on "+senderIp+" on port "+
+						port+" did not created");
+				System.out.print("$>");
+				break;
+			case ConsoleProtocol.STOPPED:
+				System.out.println("--> Chord node on "+senderIp+" on port "+
+						port+" stopped successfully");
+				System.out.print("$>");
+				Nodes tmpNode = null;
+				Nodes curNode = null;
+				Iterator<Nodes> nodesIt = nodesList.iterator();
+				while(nodesIt.hasNext()){
+					tmpNode = nodesIt.next();
+					if(tmpNode.getIpAddr().equals(senderIp) && tmpNode
+							.getPort() == port){
+						curNode = tmpNode;
+						break;
+					}
+				}
+				nodesList.remove(curNode);
+				break;
+			case ConsoleProtocol.NOT_STOPPED:
+				System.out.println("--> Chord node on "+senderIp+" on port "+
+						port+" did not stopped");
+				System.out.print("$>");
+				break;
+		}
+		bin.close();
+		}catch(IOException ex){
+			ex.printStackTrace();
 		}
 	}
 }
